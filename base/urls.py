d2c85f34a1b4394782bb90a9d5f6025a2e54d614
kamran <kamranulhaq2002@gmail.com>
2024-05-16 01:26:05 +0500

    before theme

--- base/urls.py
+++ base/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path("login",views.loginFunc,name='Login'),
     path("logout",views.logoutFunc,name="Logout"),
     path("register",views.register,name='register'),
+    path("userProfile/<str:id>",views.userProfile,name="user-profile"),
     path("room/<str:pk>",views.room,name='room'),
     path("createRoom",views.createRoom,name='create-room'),
     path("updateRoom/<int:pk>",views.updateRoom,name='update-room'),
     path("deleteRoom/<str:id>",views.deleteRoom,name='delete-room'),
+    path("deleteMessage/<str:id>",views.deleteMessage,name='delete-message'),
 ]
--- base/views.py
+++ base/views.py
@@ -60,7 +60,10 @@ def home(request):
         Q(name__icontains=q)|
         Q(description__icontains=q))
     topics = Topic.objects.all()
-    context = {'rooms':rooms,'topics':topics}
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {'rooms':rooms,'topics':topics,'room_count':room_count,"room_meesages":room_messages}
     return render(request,"base/home.html",context)
 
 
@@ -68,15 +71,19 @@ def home(request):
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by("-created")
+    participants = room.participants.all()
     if request.method == "POST":
         message = Message.objects.create(
             user = request.user,
             room = room,
             body = request.POST.get("body")
         )
+        room.participants.add(request.user)
         return redirect("room",pk=room.id)
-    context = {"room":room,'room_messages':room_messages}
+    context = {"room":room,'room_messages':room_messages,"participants":participants}
     return render(request,"base/room.html",context)
+
+
 
 @login_required(login_url="Login")
 def createRoom(request):
@@ -114,5 +121,30 @@ def deleteRoom(request,id):
         return redirect("home")
     context = {"obj":room}
     return render(request,"base/delete.html",context)
+
+
+@login_required(login_url='Login')
+def userProfile(request,id):
+    user = User.objects.get(id=id)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user,'rooms':rooms,'room_messag':room_messages,'topics':topics}
+    return render(request,"base/profile.html",context)
+
+
+
+@login_required(login_url="Login")
+def deleteMessage(request,id):
+    message = Message.objects.get(id=id)
+    if request.user != message.user:
+        return HttpResponse("you cant delete this room")
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    context = {"obj":message}
+    return render(request,"base/delete.html",context)
+
+
 
 
