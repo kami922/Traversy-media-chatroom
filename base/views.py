b030f70be915802bc14006a7e09c48ef58873d19
kamran <kamranulhaq2002@gmail.com>
2024-05-15 21:15:54 +0500

    2nd cmt

--- base/views.py
+++ base/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .models import Room,Topic
+from .models import Room,Topic,Message
 from .forms import RoomForm
 
 
@@ -67,7 +67,15 @@ def home(request):
 @login_required(login_url="Login")
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {"room":room}
+    room_messages = room.message_set.all().order_by("-created")
+    if request.method == "POST":
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get("body")
+        )
+        return redirect("room",pk=room.id)
+    context = {"room":room,'room_messages':room_messages}
     return render(request,"base/room.html",context)
 
 @login_required(login_url="Login")
